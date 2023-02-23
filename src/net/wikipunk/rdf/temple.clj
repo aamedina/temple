2d4ded846e710f1797b552c4b75d041c9e5330c3
Adrian Medina <adrian@wikipunk.net>
2023-02-23 12:15:15 -0500

    init

---
+++ src/net/wikipunk/rdf/temple.clj
@@ -0,0 +1,4 @@
+(ns net.wikipunk.rdf.temple
+  {:rdf/type :owl/Ontology})
+
+;; Begin modelling your future hyperreality today!
---
+++ src/net/wikipunk/temple/boot.clj
@@ -0,0 +1,7 @@
+(ns net.wikipunk.temple.boot
+  {:rdf/type :jsonld/Context})
+
+(def temple
+  {:rdf/type    :rdfa/PrefixMapping
+   :rdfa/uri    "https://wikipunk.net/temple/"
+   :rdfa/prefix "temple"})
